c19cc04b7b2d85dd3bbe224d3b78cfa7e55d9ff2
Pierre Beaujean <pierreb24@gmail.com>
2024-08-22 22:24:46 +0200

    Add PBLAS (#2)
    * cpblas (Fortran)
    
    * whops
    
    * test in directory
    
    * use HELLO as test
    
    * started a PBLAS test
    
    * whu
    
    * remove tests for the moment
    
    * remove release as well
    
    * remove release as well & change

--- scalapack_files_create/script.py
+++ scalapack_files_create/script.py
@@ -5,6 +5,7 @@ Create headers and wrappers for scaLAPACK
 import argparse
 from scalapack_files_create.base import get_dir
 from scalapack_files_create.create_cblacs import create_cblacs_header
+from scalapack_files_create.create_cpblas import create_cpblas_header_and_wrapper
 
 
 def main():
@@ -13,12 +14,16 @@ def main():
     parser.add_argument('-o', '--output', type=get_dir, help='Output directory', default='.')
 
     parser.add_argument('--blacs', action='store_true', help='Create BLACS header')
+    parser.add_argument('--pblas', action='store_true', help='Create PBLAS header')
     parser.add_argument('--all', action='store_true', help='Create all files')
 
     args = parser.parse_args()
 
     if args.blacs or args.all:
         create_cblacs_header(args.repo, args.output / 'cblacs.h')
+
+    if args.pblas or args.all:
+        create_cpblas_header_and_wrapper(args.repo, args.output / 'cpblas.h', args.output / 'cpblas.c')
 
 
 if __name__ == '__main__':
